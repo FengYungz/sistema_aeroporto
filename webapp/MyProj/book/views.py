5c45d8359b2b9df9bef1c74dbde1a264d0254c73
Patrick Marques de Barros Costa <patrickmarques@usp.br>
2022-10-20 15:55:01 -0300

    adicionando cadastrar

--- webapp/MyProj/book/views.py
+++ webapp/MyProj/book/views.py
@@ -1,4 +1,13 @@
+from multiprocessing import context
+import pkgutil
 from django.shortcuts import render
+from django.shortcuts import redirect
+from django.urls import reverse
+from django.shortcuts import render, get_object_or_404
+from .forms import Login, CadastrarVoo
+from django.utils import timezone
+from .models import Voo, Estado_Dinamico, Funcionario
+
 
 # Create your views here.
 def login(request):
@@ -8,6 +17,14 @@ def home(request):
     return render(request,'home.html')
 
 def cadastrar(request):
+    novoVoo = get_object_or_404(Voo, pk=pkgutil)
+    if request.method == "POST":
+        form = CadastrarVoo(request.POST)
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.data = timezone.now()
+            post.save()
+            novoVoo.save()
     return render(request,'cadastrar.html')
 
 def central(request):
