8bb4da5ea0d56b9b41ce1a02473f38561ae4d0a2
Vitor-chng <vitorchng@gmail.com>
2022-11-07 11:00:55 -0300

    adicionado edit e delete

--- webapp/MyProj/book/views.py
+++ webapp/MyProj/book/views.py
@@ -19,7 +19,7 @@ def login(request):
             post = form.save(commit=False)
             funcionario = get_object_or_404(Funcionario, cpf = post.cpf)
             if(post.senha == funcionario.senha):
-                return redirect('home')
+                return redirect('home',{'permisao':funcionario.cargo})
                 # quando o front estiver sendo feito:
                 # return redirect('home',funcionario.cargo) 
             else:
@@ -40,23 +40,22 @@ def cadastrar(request):
             post = form.save(commit=False)
             post.save()
             estado = Estado_Dinamico.objects.create(voo = post , status= 'EMB', data_saida ='0001-01-01 00:00', data_chegada ='0001-01-01 01:01')
-            print('BBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBBB')
         return redirect('cadastrar')
-    print('AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA')
     return render(request,'cadastrar.html',context)
 
 
 def central(request):
     # Listagem ainda não implementada no front
     filter = {}
-    # voos = Voo.objects.filter(filter)
-    voos = Estado_Dinamico.objects.select_related()
-    context={'voos':voos}
+    voos=Voo.objects.all()
+    voos_dinamico = Estado_Dinamico.objects.select_related()
+    context={'voos':voos,'voos_dinamico':voos_dinamico}
     return render(request, 'central.html',context)
 
 def monitorar(request):
     form = MonitorarVoo()
-    context={'form':form}
+    voos = Estado_Dinamico.objects.select_related()
+    context={'form':form,'voos':voos}
     if request.method == "POST":
         form = MonitorarVoo(request.POST)
         if form.is_valid():
@@ -74,3 +73,24 @@ def relatorio(request):
     context={'voos':voos,'voos_dinamico':voos_dinamico}
     return render(request, 'relatorio.html',context)
 
+
+
+# funcoes adicionais
+def deletar(request):
+    voos_del = request.POST['codigos']
+    voos_del = voos_del.split(",")
+    voos = Voo.objects.filter(codigo = voos_del)
+    voos.delete()
+    return redirect('central')
+
+
+def editar_voo(request):
+    form = CadastrarVoo(request.POST)
+    voo_edit = request.POST['codigos']
+    post = Voo.objects.filter(codigo = voo_edit)
+    context={'form':form}
+    if request.method == "POST":
+        if form.is_valid():
+            post = form.save(commit=False)
+            post.save()
+    return redirect('central')
